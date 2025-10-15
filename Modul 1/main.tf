8be006d45ad6e2eb14d80309912418cf0ff3d2a4
Magnus Moe <github@magnusmoe.no>
2025-10-15 17:50:34 +0200

    firstcommit

---
+++ Modul 1/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+
+  }
+  subscription_id = "a3adf20e-4966-4afb-b717-4de1baae6db1"
+}
+
+
+resource "azurerm_resource_group" "rg-sa" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "timdmsdfdsf234wrref"
+  resource_group_name      = azurerm_resource_group.rg-sa.name
+  location                 = azurerm_resource_group.rg-sa.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
